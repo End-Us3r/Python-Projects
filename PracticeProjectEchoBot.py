c99376cd8626e6ed13843695eae7498f424e9b5e
James Atkins <89127977+atkinsj5352@users.noreply.github.com>
2022-10-14 18:43:13 -0600

    Add files via upload
    Chatbot that repeats what the user types.

---
+++ PracticeProjectEchoBot.py
@@ -0,0 +1,9 @@
+greeting = "Hello, welcome to ChatBot!\n "
+
+print(greeting)
+print("This chatbot will repeat whatever the user types!\n")
+
+stuff_to_echo = input("Type anything to have the chatbot repeat it back to you! \n")
+
+print("You said: " + stuff_to_echo)
+print("Chatbot: " + stuff_to_echo)
